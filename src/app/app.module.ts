9a0162f7762ff6e0e1548b74bb04a516e066684d
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2022-10-03 17:25:21 -0500

    kev-2022-app-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
