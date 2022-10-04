6a80eca7f2edfa49aa0999d879ad201e2061fe6c
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2022-10-04 17:46:49 -0500

    kev - 2022-app-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,23 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'app';
-  public cards:Array<any> = []
+  public listdeVideos:Array<any> = []
   constructor() { }
 
   ngOnInit(): void {
   
-    this.cards = [
+    this.listdeVideos = [
       {
         title: "Video 1",
-        subtitle: "Subtitle video 1"
+        subtitle: "Subtitle video 1",
+        img: "https://i.ytimg.com/vi/73ccl96-pl8/hq720_live.jpg?sqp=CJzi8ZkG-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDfleu54KKxTrq4xTXsOzjMUEQYuA"
       },
       {
         title: "Video 2",
-        subtitle: "Subtitle video 2"
+        subtitle: "Subtitle video 2",
+        img: "https://i.ytimg.com/vi/74R8aypl9bA/hq720_live.jpg?sqp=CJzi8ZkG-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCNFUYDoVG5On2ya84zRWGlGp9eig"
+
       },
       {
         title: "Video 3",
-        subtitle: "Subtitle video 3"
+        subtitle: "Subtitle video 3",
+        img: "https://i.ytimg.com/vi/nMHbNoCMb68/hq720_live.jpg?sqp=CKz-8ZkG-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBvs0DxEgLEDU_n-7bKTAOTqRXRdA"
+
       }
     ]
 
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,11 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  @Input() dataEntrance:any;
   public image:string | undefined;
   constructor() { }
 
   ngOnInit(): void {
     this.image = 'https://image.shutterstock.com/image-photo/photo-old-movie-projector-260nw-92369284.jpg'
   }
+agregarFavorito(dataIn:any){
 
+}
 }
